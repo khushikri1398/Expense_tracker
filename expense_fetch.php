52d9cf986b9c60ef7cd0252f1eace6002c61bed5
khushikri1398 <b220052@nitsikkim.ac.in>
2024-07-07 09:46:01 +0530

    improve ui/ux

--- expense_fetch.php
+++ expense_fetch.php
@@ -15,9 +15,7 @@
     include("connection.php");
     $userprofile = $_SESSION['email'];
 
-    if ($userprofile == true) {
-
-    } else {
+    if (!$userprofile) {
         header('Location: index.php');
     }
     ?>
